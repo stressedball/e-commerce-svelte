1256ffbbfabfc4778e537a0e82a07c2b3123567e
stressedball <tserchesi.dev@gmail.com>
2023-12-26 20:22:48 +0100

    minor changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
-// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,17 +11,11 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'error.js',
-			precompress: false,
-			strict: true
-		})
+		adapter: adapter()
 	},
-	paths: {
-		base: process.argv.includes('dev') ? '' : '/e-commerce-svelte'
-	}
+	// paths: {
+	// 	base: process.argv.includes('dev') ? '' : '/e-commerce-svelte'
+	// }
 };
 
 export default config;
